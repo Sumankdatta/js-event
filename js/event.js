0ff587f85b1eccc00e6e09905cd33753ff9a2788
Sumankdatta <sumankdatta18@gmail.com>
2023-09-23 15:41:39 +0600

    event added

---
+++ js/event.js
@@ -0,0 +1,17 @@
+function makeOrange() {
+    document.body.style.backgroundColor = 'orange'
+}
+
+const makeYellow = document.getElementById('make-yellow')
+
+makeYellow.onclick = function btnYellow() {
+    document.body.style.backgroundColor = 'yellow'
+}
+
+const makePurple = document.getElementById('make purple');
+
+makePurple.onclick = btnPurple
+
+function btnPurple() {
+    document.body.style.backgroundColor = 'purple'
+}
