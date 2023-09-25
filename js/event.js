270102ce770c4abb895e1a6611f826a805c1dfc7
Sumankdatta <sumankdatta18@gmail.com>
2023-09-25 08:46:10 +0600

    deligate added

--- js/event.js
+++ js/event.js
@@ -1,13 +1,14 @@
 function makeOrange() {
     document.body.style.backgroundColor = 'orange'
 }
-
+//second
 const makeYellow = document.getElementById('make-yellow')
 
 makeYellow.onclick = function btnYellow() {
     document.body.style.backgroundColor = 'yellow'
 }
 
+//3rd
 const makePurple = document.getElementById('make purple');
 
 makePurple.onclick = btnPurple
@@ -15,3 +16,28 @@ makePurple.onclick = btnPurple
 function btnPurple() {
     document.body.style.backgroundColor = 'purple'
 }
+//forth
+const makePink = document.getElementById('make-pink')
+
+makePink.addEventListener('click', btnPink)
+
+function btnPink() {
+    document.body.style.backgroundColor = 'pink'
+}
+
+//fifth
+
+const darkRed = document.getElementById('dark-red')
+
+darkRed.addEventListener('click', function btnDarkRed() {
+    document.body.style.backgroundColor = 'DarkRed'
+})
+
+// sixth
+
+document.getElementById('make-salmon').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'Salmon'
+})
+
+
+
